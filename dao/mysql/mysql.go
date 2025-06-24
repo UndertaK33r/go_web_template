322cd6eee661c4505bf9cc05e974d88365140cf4
UndertaK33r <2331627921@qq.com>
2025-06-24 14:15:00 +0800

    v1.0

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -1,35 +1,42 @@
 package mysql
 
 import (
+	"Web_template/settings"
 	"fmt"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
-	_ "github.com/go-sql-driver/mysql" // 不要忘了导入数据库驱动
 )
 
+var db *sqlx.D
 var db *sqlx.DB
 
-func Init() (err error) {
+// Init 初始化MySQL连接
+// 参数cfg是*settings.MySQLConfig结构体指针，用于传递MySQL数据库的配置信息
+func Init(cfg *settings.MySQLConfig) (err error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-		viper.GetString("mysql.user"),
-		viper.GetString("mysql.password"),
-		viper.GetString("mysql.host"),
-		viper.GetInt("mysql.port"),
-		viper.GetString("mysql.database"),
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
 	)
 	// 也可以使用MustConnect连接不成功就panic
+	// 注意：Go的MySQL驱动有一个限制，就是数据库必须支持多语句执行，否则使用sqlx的Batch方法会报错
+	// 所以，这里我们使用了sqlx.Connect来连接MySQL，而不是sqlx.MustConnect
 	db, err = sqlx.Connect("mysql", dsn)
 	if err != nil {
 		zap.L().Error("connect DB failed", zap.Error(err))
 		return
 	}
-	db.SetMaxOpenConns(viper.GetInt("mysql.max_open_conns"))
-	db.SetMaxIdleConns(viper.GetInt("mysql.max_idle_conns"))
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
 	return
 }
 
+// 由于db小写，所以只能在本包中使用
+// 使用Close()方法关闭数据库连接,给外部调用
 func Close() {
 	db.Close()
 }
--- settings/settings.go
+++ settings/settings.go
@@ -7,18 +7,69 @@ import (
 	"github.com/spf13/viper"
 )
 
-func Init()(err error){
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
+// 全局变量，用来保存程序的所有配置信息
+// 减少viper的使用次数，直接定义一个全局变量Conf，用来保存程序的所有配置信息
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	//mapstructure是什么：mapstructure是Viper支持的配置解析器，它可以将配置信息解析到结构体中
+	//名字和yaml中的名字对应起来
+	Name    string `mapstructure:"name"`
+	Mode    string `mapstructure:"mode"`
+	Port    int    `mapstructure:"port"`
+	Version string `mapstructure:"version"`
+	// 日志配置信息
+	*LogConfig `mapstructure:"log"`
+	// MySQL配置信息
+	*MySQLConfig `mapstructure:"mysql"`
+	// Redis配置信息
+	*RedisConfig `mapstructure:"redis"`
+}
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Path       string `mapstructure:"path"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DBName       string `mapstructure:"database"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+func Init() (err error) {
+	viper.SetConfigName("config") // 指定配置文件名称（不需要带后缀）
+	viper.SetConfigType("yaml")   // 指定配置文件类型
+	viper.AddConfigPath(".")      // 指定配置文件路径
 	err = viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("viper.ReadInConfig() failed, err:%v\n",err)
-		return
+		fmt.Printf("viper.ReadInConfig() failed, err:%v\n", err)
+		return //
+	}
+	// 把读取到的配置信息反序列化到 Conf 变量中
+	// 什么是反序列化：把二进制数据转换为指定的数据结构
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
 	}
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("Config file changed:", in.Name)
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal failed, err: %v\n", err)
+		}
 	})
 	return
 }
